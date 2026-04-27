051a1873f011a22177f85c8a97fbb984b43fbd41
felix <hammy123games@gmail.com>
2026-04-27 13:31:11 +0100

    commit for vercel dep

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
-import HomePage from "./(pages)/home/page";
+import ProjectPage from "./(pages)/page";
 
 export default function Home() {
-  return (
-   <HomePage/>
-  );
+  return <ProjectPage />;
+
 }
